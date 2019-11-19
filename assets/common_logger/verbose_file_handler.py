38a789ffd1636c74d7a5a87707ce496612f2d280
Jordan Wilson <jordan.t.wilson@gmx.com>
2019-11-19 23:09:19 +0000

    In the function call inspection, programmatically find the calling frame
    Adds a inner function `get_frame' to verbose_file_handler.py's `emit()' to do this.

--- assets/common_logger/verbose_file_handler.py
+++ assets/common_logger/verbose_file_handler.py
@@ -9,18 +9,24 @@ class _VerboseFileHandler(_logging.FileHandler):
         _logging.FileHandler.__init__(self, filename, *args, **kwargs)
 
     def emit(self, record):
-        # [7] as: (0)here <- (1)`Handler.handle' in logging/__init.py__ <- (2)`Logger.callHandlers' in
-        # logging/__init.py__ <- (3)`Logger.handle' in logging/__init.py__ <- (4)`Logger._log' in logging/__init.py__
-        # <- (5) `Logger.$SEVERITY' in logging/__init.py__ <- (6) `Log.$SEVERITY' in surface/.../logger.py <- (7) call
-        # TODO: change this to be done programmatically, as just using an array access is dependent on Python's
-        #  internal details. Maybe loop down from (len(_inspect.stack()) -1) until we find "logger.py",
-        #  the stack entry after that one being the one we want.
-        caller = _inspect.getframeinfo(_inspect.stack()[7][0])
 
-        # `fileName' needs to give more of the path; treat `surface' directory as root?
-        record.fileName = os.path.basename(caller.filename)
-        # should have the parent function/class
-        record.function = caller.function
-        record.lineNum = str(caller.lineno)
+        def get_frame():
+            """Return the frame that called the logging function."""
+            stack = _inspect.stack()
+            # Traverse backwards through the stack to find the first frame that has the filename "logger.py". The frame
+            # following that is the calling frame.
+            i = len(stack) - 1
+            while i >= 0:
+                if os.path.basename(stack[i].filename) == "logger.py":
+                    return stack[i + 1][0]
+                i -= 1
+
+        caller = _inspect.getframeinfo(get_frame())
+
+        # `filename' needs to give more of the path; treat `surface' directory as root?
+        record.v_filename = os.path.basename(caller.filename)
+        # TODO should have the parent function/class
+        record.v_function = caller.function
+        record.v_lineno = caller.lineno
 
         _logging.FileHandler.emit(self, record)
