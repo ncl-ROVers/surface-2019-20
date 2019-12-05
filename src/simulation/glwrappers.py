7947e69331aeb80bec8b3252d24f8c1bb13f2cb7
EldianTitan <jason.philippou@gmail.com>
2019-12-05 16:44:44 +0000

    Fixed indentation problem and organized imports.

--- src/simulation/glwrappers.py
+++ src/simulation/glwrappers.py
@@ -5,6 +5,7 @@ GL Wrappers
 Some classes to encapsulate the creating, usage, and destruction of
 OpenGL objects.
 """
+import numpy as np
 
 # Import PyOpenGL
 import OpenGL
@@ -14,245 +15,244 @@ OpenGL.ERROR_LOGGING = True
 from OpenGL.arrays import ArrayDatatype
 from OpenGL.GL import *
 
-import numpy as np
 
 class Shader:
-	def __init__(self):
-		self.__program = 0
-		self.__shaders = []
-		self.__uniforms = {}
+    def __init__(self):
+        self.__program = 0
+        self.__shaders = []
+        self.__uniforms = {}
 
-	def init(self):
-		"""
-		Intialize shader program.
-		"""
-		self.__program = glCreateProgram();
+    def init(self):
+        """
+        Intialize shader program.
+        """
+        self.__program = glCreateProgram();
 
-	def add_shader(self, source, type):
-		"""
-		Compile the provided source and attach the result to the shader.
-		:param source: The shader source
-		:param type: The type of the shader (Vertex Shader, Fragment Shader, etc.)
-		"""
-		shader = glCreateShader(type)
-			
-		glShaderSource(shader, source)
-		glCompileShader(shader)
+    def add_shader(self, source, type):
+        """
+        Compile the provided source and attach the result to the shader.
+        :param source: The shader source
+        :param type: The type of the shader (Vertex Shader, Fragment Shader, etc.)
+        """
+        shader = glCreateShader(type)
+            
+        glShaderSource(shader, source)
+        glCompileShader(shader)
 
-		result = glGetShaderiv(shader, GL_COMPILE_STATUS)
-		if not glGetShaderiv(shader, GL_COMPILE_STATUS) == GL_TRUE:
-			raise RuntimeError(glGetShaderInfoLog(shader))
+        result = glGetShaderiv(shader, GL_COMPILE_STATUS)
+        if not glGetShaderiv(shader, GL_COMPILE_STATUS) == GL_TRUE:
+            raise RuntimeError(glGetShaderInfoLog(shader))
 
-		glAttachShader(self.__program, shader)
+        glAttachShader(self.__program, shader)
 
-		self.__shaders.append(shader)
+        self.__shaders.append(shader)
 
-	def compile(self):
-		"""
-		Compile the entire shader program and check fir compilation errors.
-		"""
-		glLinkProgram(self.__program)
-		glValidateProgram(self.__program)
+    def compile(self):
+        """
+        Compile the entire shader program and check fir compilation errors.
+        """
+        glLinkProgram(self.__program)
+        glValidateProgram(self.__program)
 
-		if not glGetProgramiv(self.__program, GL_LINK_STATUS) == GL_TRUE:
-			raise RuntimeError(glGetProgramInfoLog(self.__program))
+        if not glGetProgramiv(self.__program, GL_LINK_STATUS) == GL_TRUE:
+            raise RuntimeError(glGetProgramInfoLog(self.__program))
 
-		for shader in self.__shaders:
-			glDeleteShader(shader)
+        for shader in self.__shaders:
+            glDeleteShader(shader)
 
-	def bind(self):
-		"""
-		Bind the shader.
-		"""
-		glUseProgram(self.__program)
+    def bind(self):
+        """
+        Bind the shader.
+        """
+        glUseProgram(self.__program)
 
-	def unbind(self):
-		"""
-		Unbind the shader.
-		"""
-		glUseProgram(0)
+    def unbind(self):
+        """
+        Unbind the shader.
+        """
+        glUseProgram(0)
 
-	def get_uniform_location(self, uniform_name) -> int:
-		"""
-		Get the location of the uniform with the given name.
-		:param uniform_name: The name of the uniform
-		:return: The location of the uniform
-		"""
-		location = self.__uniforms.get(uniform_name, -1)
+    def get_uniform_location(self, uniform_name) -> int:
+        """
+        Get the location of the uniform with the given name.
+        :param uniform_name: The name of the uniform
+        :return: The location of the uniform
+        """
+        location = self.__uniforms.get(uniform_name, -1)
 
-		if location == -1:
-			location = glGetUniformLocation(self.__program, uniform_name)
-			self.__uniforms[uniform_name] = location
+        if location == -1:
+            location = glGetUniformLocation(self.__program, uniform_name)
+            self.__uniforms[uniform_name] = location
 
-		return location
+        return location
 
-	def set_uniform_1f(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform1f(location, value)
+    def set_uniform_1f(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform1f(location, value)
 
-	def set_uniform_2f(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform2f(location, value[0], value[1])
+    def set_uniform_2f(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform2f(location, value[0], value[1])
 
-	def set_uniform_3f(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform3f(location, value[0], value[1], value[2])
+    def set_uniform_3f(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform3f(location, value[0], value[1], value[2])
 
-	def set_uniform_4f(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform4f(location, value[0], value[1], value[2], value[3])
+    def set_uniform_4f(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform4f(location, value[0], value[1], value[2], value[3])
 
-	def set_uniform_1i(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform1i(location, value)
+    def set_uniform_1i(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform1i(location, value)
 
-	def set_uniform_2i(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform2i(location, value[0], value[1])
+    def set_uniform_2i(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform2i(location, value[0], value[1])
 
-	def set_uniform_3i(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform3i(location, value[0], value[1], value[2])
+    def set_uniform_3i(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform3i(location, value[0], value[1], value[2])
 
-	def set_uniform_4i(self, name, value):
-		"""
-		Set uniform value.
-		:param name: The name of the uniform
-		:param value: The value to be assigned to the uniform
-		"""
-		location = self.get_uniform_location(name)
-		glUniform4i(location, value[0], value[1], value[2], value[3])
+    def set_uniform_4i(self, name, value):
+        """
+        Set uniform value.
+        :param name: The name of the uniform
+        :param value: The value to be assigned to the uniform
+        """
+        location = self.get_uniform_location(name)
+        glUniform4i(location, value[0], value[1], value[2], value[3])
 
-	def destroy(self):
-		"""
-		Destroy the shader program.
-		"""
-		glDeleteProgram(self.__program)
+    def destroy(self):
+        """
+        Destroy the shader program.
+        """
+        glDeleteProgram(self.__program)
 
 
 class VertexArray:
-	def __init__(self):
-		self.__vao = 0
-		self.__vbo_list = []
+    def __init__(self):
+        self.__vao = 0
+        self.__vbo_list = []
 
-	def init(self):
-		"""
-		Initialize vertex array.
-		"""
-		self.__vao = glGenVertexArrays(1)
-		glBindVertexArray(self.__vao)
+    def init(self):
+        """
+        Initialize vertex array.
+        """
+        self.__vao = glGenVertexArrays(1)
+        glBindVertexArray(self.__vao)
 
-	def create_buffer(self, data, size) -> int:
-		"""
-		Add a VBO ti the vertex array.
-		:param data: An array containing the data to be copied into the buffer
-		:param size: The size of the buffer
-		:return: The index of the buffer that was created
-		"""
-		vbo = glGenBuffers(1)
-		glBindBuffer(GL_ARRAY_BUFFER, vbo)
-		glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW)
+    def create_buffer(self, data, size) -> int:
+        """
+        Add a VBO ti the vertex array.
+        :param data: An array containing the data to be copied into the buffer
+        :param size: The size of the buffer
+        :return: The index of the buffer that was created
+        """
+        vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW)
 
-		vbo_index = len(self.__vbo_list)
-		self.__vbo_list.append(vbo)
+        vbo_index = len(self.__vbo_list)
+        self.__vbo_list.append(vbo)
 
-		return vbo_index
+        return vbo_index
 
-	def bind_attribute(self, buffer_index, attrib_index, component_count, type, normalized, stride, offset):
-		"""
-		Add a vertex attribute to the specified buffer.
-		:param buffer_index: The index of the buffer to add the attribute to
-		:param attrib_index: The index of the attribute
-		:param component_type: The number of components of the attribute
-		:param type: The type of the attribute (float, int, etc.)
-		:param normalized: Should the provided data be normalized.
-		:param stride: The number of bytes between consecutive attribute values
-		:param offset: The offset into the buffer of the first element.
-		"""
-		vbo = self.get_buffer(buffer_index)
-		if vbo == 0:
-			return
+    def bind_attribute(self, buffer_index, attrib_index, component_count, type, normalized, stride, offset):
+        """
+        Add a vertex attribute to the specified buffer.
+        :param buffer_index: The index of the buffer to add the attribute to
+        :param attrib_index: The index of the attribute
+        :param component_type: The number of components of the attribute
+        :param type: The type of the attribute (float, int, etc.)
+        :param normalized: Should the provided data be normalized.
+        :param stride: The number of bytes between consecutive attribute values
+        :param offset: The offset into the buffer of the first element.
+        """
+        vbo = self.get_buffer(buffer_index)
+        if vbo == 0:
+            return
 
-		glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
 
-		glEnableVertexAttribArray(attrib_index)
-		glVertexAttribPointer(attrib_index, component_count, type, GL_TRUE if normalized else GL_FALSE, stride, ctypes.c_void_p(offset))
+        glEnableVertexAttribArray(attrib_index)
+        glVertexAttribPointer(attrib_index, component_count, type, GL_TRUE if normalized else GL_FALSE, stride, ctypes.c_void_p(offset))
 
-	def get_buffer(self, index) -> int:
-		"""
-		Get a buffer attached to this vertex array.
-		:param index: The index of the buffer to be retreived
-		:return: The OpenGL handle of the buffer at the specified index
-		"""
-		if index < 0 or index >= len(self.__vbo_list):
-			return 0
-		return self.__vbo_list[index]
+    def get_buffer(self, index) -> int:
+        """
+        Get a buffer attached to this vertex array.
+        :param index: The index of the buffer to be retreived
+        :return: The OpenGL handle of the buffer at the specified index
+        """
+        if index < 0 or index >= len(self.__vbo_list):
+            return 0
+        return self.__vbo_list[index]
 
-	def bind(self):
-		"""
-		Bind the vertex array.
-		"""
-		glBindVertexArray(self.__vao)
+    def bind(self):
+        """
+        Bind the vertex array.
+        """
+        glBindVertexArray(self.__vao)
 
-	def bind_buffer(self, index):
-		"""
-		Bind the specified vertex buffer.
-		:param index: The index of the buffer to be bound
-		"""
-		glBindBuffer(GL_ARRAY_BUFFER, self.get_buffer(index))
+    def bind_buffer(self, index):
+        """
+        Bind the specified vertex buffer.
+        :param index: The index of the buffer to be bound
+        """
+        glBindBuffer(GL_ARRAY_BUFFER, self.get_buffer(index))
 
-	def unbind_buffers(self):
-		"""
-		Unbind the currently bound vertex buffer.
-		"""
-		glBindBuffer(GL_ARRAY_BUFFER, 0)
+    def unbind_buffers(self):
+        """
+        Unbind the currently bound vertex buffer.
+        """
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
 
-	def unbind(self):
-		"""
-		Unbind the currently bound vertex array.
-		"""
-		self.unbind_buffers()
+    def unbind(self):
+        """
+        Unbind the currently bound vertex array.
+        """
+        self.unbind_buffers()
 
-		glBindVertexArray(0)
+        glBindVertexArray(0)
 
-	def destroy(self):
-		"""
-		Destroy the vertex array and all of its attached buffers.
-		"""
-		glDeleteBuffers(len(self.__vbo_list), np.array(self.__vbo_list))
-		glDeleteVertexArrays(1, np.array([self.__vao]))
+    def destroy(self):
+        """
+        Destroy the vertex array and all of its attached buffers.
+        """
+        glDeleteBuffers(len(self.__vbo_list), np.array(self.__vbo_list))
+        glDeleteVertexArrays(1, np.array([self.__vao]))
--- src/simulation/simcore.py
+++ src/simulation/simcore.py
@@ -4,9 +4,15 @@ SimCore
 
 TODO: Add documentation
 """
-
 from ..common import Log
 from .glwrappers import *
+
+import glfw
+
+import threading
+import time
+import numpy as np
+import math
 
 # Import PyOpenGL
 import OpenGL
@@ -16,13 +22,6 @@ OpenGL.ERROR_LOGGING = True
 from OpenGL.arrays import ArrayDatatype
 from OpenGL.GL import *
 
-# Import GLFW
-import glfw
-
-import threading
-import time
-import numpy as np
-import math
 
 vertex_shader_code = """
 #version 430 core
@@ -33,8 +32,8 @@ layout(location = 1) in vec2 texCoords;
 layout(location = 0) out vec2 outTexCoords;
 
 void main() {
-	outTexCoords = texCoords;
-	gl_Position = vec4(position, 0.0, 1.0);
+    outTexCoords = texCoords;
+    gl_Position = vec4(position, 0.0, 1.0);
 }
 """
 
@@ -48,181 +47,177 @@ layout(location = 0) in vec2 inTexCoords;
 uniform float time;
 
 void main() {
-	outColor = vec4(inTexCoords * time, 0.5, 1.0);
+    outColor = vec4(inTexCoords * time, 0.5, 1.0);
 }
 """
 
+
 class SimEngine:
-	def __init__(self):
-		self.__running = True
-		self.__window = None
+    def __init__(self):
+        self.__running = True
+        self.__window = None
 
-		self.__test_mouse = (0, 0)
+        self.__test_mouse = (0, 0)
 
-		self.__vao = 0
-		self.__shader = None
+        self.__vao = 0
+        self.__shader = None
 
-	@classmethod
-	def __start(self, title, width, height):
-		"""
-		Create window, initialize simulation and load resources.
-		:param title: The title to be displayed on the window border
-		:param width: The width that the window will have when created
-		:param height: The height that the window will have when created
-		"""
-		Log.debug("Intializing simulation")
+    def __init_resources(self, title, width, height):
+        """
+        Create window, initialize simulation and load resources.
+        :param title: The title to be displayed on the window border
+        :param width: The width that the window will have when created
+        :param height: The height that the window will have when created
+        """
+        Log.debug("Intializing simulation")
 
-		# Window creation
-		if not glfw.init():
-			Log.error("Failed to intialize GLFW!");
-			quit()
-		
-		glfw.window_hint(glfw.VISIBLE, glfw.TRUE)
-		glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
-		glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-		glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-		glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, glfw.TRUE)
+        # Window creation
+        if not glfw.init():
+            Log.error("Failed to intialize GLFW!");
+            quit()
 
-		self.__window = glfw.create_window(width, height, title, None, None)
-		if not self.__window:
-			Log.error("GLFW window creation failed")
-			glfw.terminate()
-			quit()
+        glfw.window_hint(glfw.VISIBLE, glfw.TRUE)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, glfw.TRUE)
 
-		glfw.make_context_current(self.__window)
-		glfw.swap_interval(1)
+        self.__window = glfw.create_window(width, height, title, None, None)
+        if not self.__window:
+            Log.error("GLFW window creation failed")
+            glfw.terminate()
+            quit()
 
-    	# Resource initialiation
-		Log.debug("OpenGL Version: {}".format(glGetString(GL_VERSION)))
+        glfw.make_context_current(self.__window)
+        glfw.swap_interval(1)
 
-		glViewport(0, 0, width, height)
+        # Resource initialiation
+        Log.debug("OpenGL Version: {}".format(glGetString(GL_VERSION)))
 
-		# Shader setup
-		shader = Shader()
+        glViewport(0, 0, width, height)
 
-		shader.init()
-		shader.add_shader(vertex_shader_code, GL_VERTEX_SHADER)
-		shader.add_shader(fragment_shader_code, GL_FRAGMENT_SHADER)
-		shader.compile()
+        # Shader setup
+        shader = Shader()
 
-		self.__shader = shader
+        shader.init()
+        shader.add_shader(vertex_shader_code, GL_VERTEX_SHADER)
+        shader.add_shader(fragment_shader_code, GL_FRAGMENT_SHADER)
+        shader.compile()
 
-		# Buffer setup
-		vertices = np.array([-0.7, 0.7, 0.0, 1.0,
-							 -0.7, -0.7, 0.0, 0.0,
-							 0.7, 0.7, 1.0, 1.0,
-							 -0.7, -0.7, 0.0, 0.0,
-							 0.7, -0.7, 1.0, 0.0,
-							 0.7, 0.7, 1.0, 1.0], dtype=np.float32)
+        self.__shader = shader
 
-		vao = VertexArray()
-		vao.init()
-		vao.create_buffer(vertices, 6 * 4 * 4)
-		vao.bind_attribute(0, 0, 2, GL_FLOAT, False, 4 * 4, 0)
-		vao.bind_attribute(0, 1, 2, GL_FLOAT, False, 4 * 4, 2 * 4)
+        # Buffer setup
+        vertices = np.array([-0.7, 0.7, 0.0, 1.0,
+                             -0.7, -0.7, 0.0, 0.0,
+                             0.7, 0.7, 1.0, 1.0,
+                             -0.7, -0.7, 0.0, 0.0,
+                             0.7, -0.7, 1.0, 0.0,
+                             0.7, 0.7, 1.0, 1.0], dtype=np.float32)
 
-		self.__vao = vao
+        vao = VertexArray()
+        vao.init()
+        vao.create_buffer(vertices, 6 * 4 * 4)
+        vao.bind_attribute(0, 0, 2, GL_FLOAT, False, 4 * 4, 0)
+        vao.bind_attribute(0, 1, 2, GL_FLOAT, False, 4 * 4, 2 * 4)
 
-	@classmethod
-	def __update(self, delta):
-		"""
-		Update the state of the engine.
-		:param delta: The time in seconds since the last update occurred
-		"""
-		pass
+        self.__vao = vao
 
-	@classmethod
-	def __render(self):
-		"""
-		Render what the camera is seeing onto the window.
-		"""
-		glClearColor(0.0, 0.0, 0.0, 1.0)
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    def __update(self, delta):
+        """
+        Update the state of the engine.
+        :param delta: The time in seconds since the last update occurred
+        """
+        pass
 
-		# Test rendering code
-		self.__shader.bind()
-		self.__shader.set_uniform_1f("time", 0.5 * math.sin(time.time()) + 0.5)
+    def __render(self):
+        """
+        Render what the camera is seeing onto the window.
+        """
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-		self.__vao.bind()
-		glDrawArrays(GL_TRIANGLES, 0, 6)
+        # Test rendering code
+        self.__shader.bind()
+        self.__shader.set_uniform_1f("time", 0.5 * math.sin(time.time()) + 0.5)
 
-		# Present
-		glfw.swap_buffers(self.__window)
+        self.__vao.bind()
+        glDrawArrays(GL_TRIANGLES, 0, 6)
 
-	@classmethod
-	def __handle_event(self, event):
-		"""
-		Handle events sent by the window.
-		:param event: The event to be processed
-		"""
-		pass
+        # Present
+        glfw.swap_buffers(self.__window)
 
-	@classmethod
-	def __exit(self):
-		"""
-		Perform all necessary cleanup before terminating the simulation.
-		"""
-		Log.debug("Exiting simulation");
+    def __handle_event(self, event):
+        """
+        Handle events sent by the window.
+        :param event: The event to be processed
+        """
+        pass
 
-		self.__vao.destroy()
-		self.__shader.destroy()
+    def __exit(self):
+        """
+        Perform all necessary cleanup before terminating the simulation.
+        """
+        Log.debug("Exiting simulation");
 
-		glfw.destroy_window(self.__window)
-		glfw.terminate()
+        self.__vao.destroy()
+        self.__shader.destroy()
 
-	@classmethod
-	def run(self, title, width, height, framerate):
-		"""
-		Execute the core engine loop. Initialization, updating, rendering, event handling
-		and termination are managed by the engine loop.
-		:param title: The title to be displayed on the window border
-		:param width: The width that the window will have when created
-		:param height: The height that the window will have when created
-		"param framerate: The maximum number of frames per second allowed
-		"""
-		self.__start(title, width, height)
+        glfw.destroy_window(self.__window)
+        glfw.terminate()
 
-		time_millis = lambda: int(round(time.time() * 1000))
-		frame_time = 1.0 / framerate * 1000.0
-		frame_count = 0
+    def run(self, title, width, height, framerate):
+        """
+        Execute the core engine loop. Initialization, updating, rendering, event handling
+        and termination are managed by the engine loop.
+        :param title: The title to be displayed on the window border
+        :param width: The width that the window will have when created
+        :param height: The height that the window will have when created
+        "param framerate: The maximum number of frames per second allowed
+        """
+        self.__init_resources(title, width, height)
 
-		last_time = time_millis()
-		last_render_time = time_millis()
-		last_tick_time = time_millis()
+        time_millis = lambda: int(round(time.time() * 1000))
+        frame_time = 1.0 / framerate * 1000.0
+        frame_count = 0
 
-		self.__running = True
-		while self.__running:
-			# Handle events
-			glfw.poll_events()
-			self.__running = not glfw.window_should_close(self.__window)
-			# self.__handle_event(event)
+        last_time = time_millis()
+        last_render_time = time_millis()
+        last_tick_time = time_millis()
 
-			elapsed_time = time_millis() - last_time
-			last_time = time_millis()
+        self.__running = True
+        while self.__running:
+            # Handle events
+            glfw.poll_events()
+            self.__running = not glfw.window_should_close(self.__window)
+            # self.__handle_event(event)
 
-			# Update
-			self.__update(elapsed_time / 1000.0)
+            elapsed_time = time_millis() - last_time
+            last_time = time_millis()
 
-			# Render
-			if (time_millis() - last_render_time) >= frame_time:
-				last_render_time = time_millis()
-				frame_count += 1
+            # Update
+            self.__update(elapsed_time / 1000.0)
 
-				self.__render()
-			else:
-				time.sleep(0.001)
+            # Render
+            if (time_millis() - last_render_time) >= frame_time:
+                last_render_time = time_millis()
+                frame_count += 1
 
-			# This is executed once a second
-			if (time_millis() - last_tick_time) >= 1000:
-				last_tick_time = time_millis()
-				frame_count = 0
+                self.__render()
+            else:
+                time.sleep(0.001)
 
-		self.__exit()
+            # This is executed once a second
+            if (time_millis() - last_tick_time) >= 1000:
+                last_tick_time = time_millis()
+                frame_count = 0
+
+        self.__exit()
+
 
 def init():
-	def core():
-		sim = SimEngine()
-		sim.run("ROV Simulation", 1280, 720, 60)
+    def core():
+        sim = SimEngine()
+        sim.run("ROV Simulation", 1280, 720, 60)
 
-	thread = threading.Thread(target=core, args=())
-	thread.start()
+    thread = threading.Thread(target=core, args=())
+    thread.start()
