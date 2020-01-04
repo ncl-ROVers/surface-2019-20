4f3e3ed5e80f65f07c26fcc8130b2a58b1e86908
Kacper Florianski <36332694+TheCodeSummoner@users.noreply.github.com>
2020-01-04 13:49:42 +0100

    pytest integration
    Moved and updated docs
    Fixed and rearranged tests
    Fixed some common code paths

--- src/common/utils.py
+++ src/common/utils.py
@@ -13,16 +13,18 @@ ROOT_DIR = _os.path.normpath(_os.path.join(_os.path.dirname(__file__), "..", "..
 ASSETS_DIR = _os.path.join(ROOT_DIR, "assets")
 SRC_DIR = _os.path.join(ROOT_DIR, "src")
 LOG_DIR = _os.path.join(ROOT_DIR, "log")
+TESTS_DIR = _os.path.join(ROOT_DIR, "tests")
 
 # Declare the paths to the source paths
 SRC_COMMON_DIR = _os.path.join(SRC_DIR, "common")
 SRC_GUI_DIR = _os.path.join(SRC_DIR, "gui")
-SRC_LOG_DIR = _os.path.join(SRC_DIR, "log")
+SRC_CONTROL_DIR = _os.path.join(SRC_DIR, "control")
 
 # Declare the paths to the assets folders
 GUI_LOADING_DIR = _os.path.join(ASSETS_DIR, "gui_loading")
 COMMON_LOGGER_DIR = _os.path.join(ASSETS_DIR, "common_logger")
 COMMON_LOCKS_DIR = _os.path.join(ASSETS_DIR, "common_locks")
+TESTS_ASSETS_DIR = _os.path.join(TESTS_DIR, "assets")
 
 # Declare shared memory data mappings
 TRANSMISSION_DICT = {
