f069377c2a4c18031107d398def6723556c58947
Dominykas Mickevicius <dominykas.mickev@gmail.com>
2020-01-09 17:30:55 +0000

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 import setuptools
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="ncl-rovers",
