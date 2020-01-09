dd8fcf48e8b674c80accf86a795c9f337f119479
Kacper <k.florianski@newcastle.ac.uk>
2020-01-09 20:31:26 +0100

    Final version

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
+"""
+Standard setup.py file.
+"""
 import setuptools
-from os import path
-from io import open
+import os
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# TODO: Restructure the project (or implement alternative solutions) to allow "python pip install ." <- packages=[???]
 setuptools.setup(
     name="ncl-rovers",
-    version="1.0",
+    version="1.0.dev1",
     author="Newcastle University Engineering Projects Society",
-    description="Newcastle University ROV project for MATE competition",
+    maintainer="Florianski Kacper",
+    maintainer_email="k.florianski@newcastle.ac.uk",
+    description="ROV project for MATE competition",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ncl-ROVers",
+    license="MIT License",
     packages=[],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
     ],
     install_requires=[
@@ -25,7 +29,7 @@ setuptools.setup(
         'inputs',
         'PySide2',
         'pyautogui',
-	'pytest',
-        ],
-    python_requires='>=3.8',
+        'pytest',
+    ],
+    python_requires='>=3.8.1',
 )
