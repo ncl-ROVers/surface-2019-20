97b94a3a8fdab2433d7029384b85909f135983c6
Kacper <k.florianski@newcastle.ac.uk>
2020-02-15 21:44:13 +0000

    Moved the code, added TODO-s to resolve, adjusted setup to download dependencies,

--- src/vision/mussels.py
+++ src/vision/mussels.py
@@ -1,3 +1,9 @@
+"""
+Mussels counting
+================
+
+Module storing an implementation of the mussels counting task.
+"""
 from sklearn.cluster import KMeans as _KMeans
 import pandas as _pd
 import cv2 as _cv2
@@ -5,12 +11,12 @@ import numpy as _np
 import typing as _typing
 
 
-def _clean_circles(mask):
+def _remove_circles(mask: _np.ndarray) -> _np.ndarray:
     """
-    Clean the small circles in the graph
+    Remove the small circles (mussels) in the image.
 
-    :param mask: the mask after HSV filter
-    :return: the mask after cleaning
+    :param mask: Mask after HSV filter
+    :return: Mask after cleaning
     """
     contours, hi = _cv2.findContours(mask, _cv2.RETR_TREE, _cv2.CHAIN_APPROX_NONE)
 
@@ -22,62 +28,62 @@ def _clean_circles(mask):
     return mask
 
 
-def _gaussian_blur_smooth(mask):
+def _gaussian_blur_smooth(mask: _np.ndarray) -> _np.ndarray:
     """
-    Convert the image to canny line with Gaussian blur
+    Convert the image to Canny Line with Gaussian blur.
 
-    :param mask: the mask of the target
-    :return: canny line with Gaussian blur
+    :param mask: Mask with mussels removed
+    :return: Canny Line with Gaussian blur
     """
     # Erode the image
     kernel = _np.ones((7, 7), _np.uint8)
     mask = _cv2.erode(mask, kernel, iterations=1)
 
     # Canny transform to get the edge of the image
-    img_canny = _cv2.Canny(mask, 20, 250)
+    canny = _cv2.Canny(mask, 20, 250)
 
     # Gaussian blur to smooth the edge to get the Hough line easier
-    img_gaussian_blur = _cv2.GaussianBlur(img_canny, (5, 5), 0)
-    return img_gaussian_blur
+    blurred = _cv2.GaussianBlur(canny, (5, 5), 0)
+    return blurred
 
 
-def _get_edge_points(img_gaussian_blur):
+def _get_edge_points(mask) -> list:
     """
     Get the list of points on the edge of the square
 
-    :param img_gaussian_blur: the canny line after Gaussian blur
-    :return: the list of points on the edge of the square
+    :param mask: Canny Line after Gaussian blur
+    :return: List of points on the edge of the square
     """
-    # Get the points of the Hough line
-    lines = _cv2.HoughLinesP(img_gaussian_blur, rho=0.1, theta=_np.pi / 90, threshold=4, lines=4, minLineLength=200,
-                             maxLineGap=50)
+    lines = _cv2.HoughLinesP(mask, rho=0.1, theta=_np.pi / 90, threshold=4, lines=4, minLineLength=200, maxLineGap=50)
+
     # Attach the points to a list
-    lines1 = lines[:, 0, :]
+    lines = lines[:, 0, :]
     points = list()
-    for x1, y1, x2, y2 in lines1:
+    for x1, y1, x2, y2 in lines:
         points.append((x1, y1))
         points.append((x2, y2))
+    print(points)
     return points
 
 
-def _find_points(points):
+def _get_corner_points(points: list) -> _np.ndarray:
     """
     Find the points on four edge by K-Means Cluster
 
-    :param points: the list of points on the edge of the square
-    :return: the list of four edge points with a convex hull order
+    :param points: List of points on the edge of the square
+    :return: List of four edge points with a convex hull order
     """
-    # Create Pandas.DataFrame for analyze
+    # Create Pandas.DataFrame for analysis
     rect_points = _pd.DataFrame(points)
     rect_points = rect_points.rename(columns={0: 'x', 1: 'y'})
 
-    # Use K-Means Cluster to classify different points(Four corner points)
+    # Use K-Means Cluster to classify different points (four corner points)
     km = _KMeans(n_clusters=4).fit(rect_points)
     rect_points['label'] = km.labels_
     _pd.set_option('max_rows', 100)
     rect_points.sort_values('label')
 
-    # Store the different type of points
+    # Store different types of points
     edge_points = list()
     for i in range(4):
         df_all = _drop_noisy(rect_points[rect_points['label'] == i]).mean()
@@ -87,17 +93,18 @@ def _find_points(points):
     rect = _np.array(edge_points, _np.int32)
     rect = rect.reshape((-1, 1, 2))
     hull_rect = _cv2.convexHull(rect)
+
     return hull_rect
 
 
-def _find_circles(img_grey, image, hull_rect):
+def _find_mussels(img_grey: _np.ndarray, mask: _np.ndarray, hull_rect: _np.ndarray):
     """
     Find, count and draw the circles on the image
 
-    :param img_grey: the image in grey color
-    :param image: the original image
-    :param hull_rect: the convex hull points
-    :return: the image after drawing, the number of circles
+    :param img_grey: Image in grey scale
+    :param mask: Original image
+    :param hull_rect: Convex hull points
+    :return: Image after drawing, Number of circles
     """
     # Find the circles in the image
     circles = _cv2.HoughCircles(img_grey, method=_cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=10, param2=8,
@@ -108,20 +115,19 @@ def _find_circles(img_grey, image, hull_rect):
         i = i.astype(_np.int32)
         if _cv2.pointPolygonTest(hull_rect, (i[0], i[1]), measureDist=True) > (-i[2] / 3):
             # draw the outer circle
-            _cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            _cv2.circle(mask, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
-            _cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
+            _cv2.circle(mask, (i[0], i[1]), 2, (0, 0, 255), 3)
 
-    _cv2.drawContours(image, [hull_rect], 0, (0, 0, 255), 1)
-    return image, len(circles[0])
+    return mask, len(circles[0])
 
 
 def _drop_noisy(df: _pd.DataFrame) -> _pd.DataFrame:
     """
     Filter the outlier points in the data by delete the points lower than a certain area.
 
-    :param df: the dataframe of the data
-    :return: the dataframe after filter
+    :param df: DataFrame of the data
+    :return: DataFrame after filter
     """
     df_copy = df.copy()
     df_describe = df_copy.describe()
@@ -135,42 +141,60 @@ def _drop_noisy(df: _pd.DataFrame) -> _pd.DataFrame:
     return df_copy
 
 
-def count_mussels(image: _np.ndarray) -> _typing.Tuple[_np.ndarray, _np.ndarray, int]:
+def count_mussels(image: _np.ndarray) -> \
+        _typing.Tuple[_np.ndarray, _np.ndarray, _np.ndarray, _np.ndarray, _np.ndarray, int]:
     """
-    Display and count the number of the mussels in the square.
+    Count the number of the mussels in the square.
 
-    :param image: the input image
-    :return: the result graph, the original graph, the number of mussels
+    Returns the following sub-images:
+
+        1. Original
+        2. Filtered with circles removed
+        3. Blurred and smoothed
+        4. Convex hull (4 corners)
+        5. Mussels found
+
+    and the number of mussels detected.
+
+    :param image: Input image
+    :return: Images: Original, Circles removed, as well as the number of mussels
     """
-    img_hsv = _cv2.cvtColor(image, _cv2.COLOR_BGR2HSV)
+    hsv = _cv2.cvtColor(image, _cv2.COLOR_BGR2HSV)
 
     # Set the range of the HSV field to create the mask
     lower = _np.array([0, 0, 220])
     upper = _np.array([255, 50, 255])
-    mask = _cv2.inRange(img_hsv, lower, upper)
-    img_grey = _cv2.inRange(img_hsv, lower, upper)
+    grey = _cv2.inRange(hsv, lower, upper)
 
-    # clean the small circle in the mask
-    mask = _clean_circles(mask)
+    # Remove the mussels from the image
+    circles_removed = _remove_circles(grey.copy())
 
     # Gaussian blur to smooth the edge to get the Hough line easier
-    img_gaussian_blur = _gaussian_blur_smooth(mask)
+    blurred_and_smoothed = _gaussian_blur_smooth(grey.copy())
 
-    # get the list of points on the edge of the square
-    points = _get_edge_points(img_gaussian_blur)
+    # Get the list of points on the edge of the square
+    points = _get_edge_points(blurred_and_smoothed)
 
-    # find the points on four edge by K-Means Cluster
-    hull_rect = _find_points(points)
+    # Find the points on four edges, using K-Means Cluster
+    hull_rect = _get_corner_points(points)
+
+    # Draw hull rect on the original image
+    convex_hull = image.copy()
+    _cv2.drawContours(convex_hull, [hull_rect], 0, (0, 0, 255), 3)
 
     # find, count and draw the circles and square on the image
-    image_drawing, circles_num = _find_circles(img_grey, image, hull_rect)
+    mussels_found, mussels_count = _find_mussels(grey, image.copy(), hull_rect)
 
-    return image_drawing, image, circles_num
+    return image, circles_removed, blurred_and_smoothed, convex_hull, mussels_found, mussels_count
 
 
-# TODO: This will be a part of test rather than module code - Testing team will adjust this part
-read_image = _cv2.imread('./catch.PNG')
-image_result, image_original, result = count_mussels(read_image)
-_cv2.imshow('rect', image_result)
-print(result)
+# TODO: This will be a part of test/example code rather than module code - Testing team will adjust this part
+img = _cv2.imread('./catch.PNG')
+_orig, _circles_removed, _blurred_and_smoothed, _convex_hull, _mussels_found, _mussels_count = count_mussels(img)
+_cv2.imshow("1. Original", _orig)
+_cv2.imshow("2. Circles removed", _circles_removed)
+_cv2.imshow("3. Blurred and smoothed", _blurred_and_smoothed)
+_cv2.imshow("4. Convex hull", _convex_hull)
+_cv2.imshow("5. Mussels", _mussels_found)
+print("Counted", _mussels_count, "mussels")  # TODO: Number of mussels is wrong
 _cv2.waitKey(0)
